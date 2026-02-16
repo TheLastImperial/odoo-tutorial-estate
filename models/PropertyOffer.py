652fe0869222278dfb18a20e424cf8a4dff5d15d
TheLastImperial <tli@thelastimperial.com>
2026-02-16 00:09:38 -0700

    Unit 8

--- models/PropertyOffer.py
+++ models/PropertyOffer.py
@@ -1,4 +1,7 @@
-from odoo import fields, models
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models
 class PropertyOffer(models.Model):
     _name = "estate.property.offer"
     _description = """
@@ -11,5 +14,29 @@ class PropertyOffer(models.Model):
             ("accepted", "Accepted"), ("refused", "Refused")
         ]
     )
-    partner_id = fields.Many2one("res.partner", string="Partner", required=True)
+    partner_id = fields.Many2one("res.partner",
+        string="Partner",
+        required=True
+    )
     property_id = fields.Many2one("estate.property", string="Property", required=True)
+    validity = fields.Integer(default = 7)
+    date_deadline = fields.Date(
+        compute="_compute_validity_date",
+        inverse="_inverse_date_deadline"
+    )
+
+    @api.depends("validity")
+    def _compute_validity_date(self):
+        for record in self:
+            if record.create_date:
+                record.date_deadline = record.create_date + relativedelta(
+                    days=record.validity
+                )
+            else:
+                record.date_deadline = datetime.today() + relativedelta(days=record.validity)
+    def _inverse_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.validity = (record.date_deadline - record.create_date.date()).days
+            else:
+                record.validity = (record.date_deadline - datetime.now().date()).days
